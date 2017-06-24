ca029101d152706b13e13d0f9c9de547b42a1f35
MartaPiatek <martusia.piatek@gmail.com>
2017-06-24 16:41:10 +0200

    Dodanie w menu paska akcji poleceń nowe przypomnienie i wyjście

--- app/src/main/java/pl/martapiatek/reminders/RemindersActivity.java
+++ app/src/main/java/pl/martapiatek/reminders/RemindersActivity.java
@@ -2,6 +2,9 @@ package pl.martapiatek.reminders;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -29,4 +32,27 @@ public class RemindersActivity extends AppCompatActivity {
         mListView.setAdapter(arrayAdapter);
 
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_reminders, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_new:
+                //utworzenie nowego przypomninia
+                Log.d(getLocalClassName(), "utworzenie nowego przypomnienia");
+                return true;
+            case R.id.action_exit:
+                finish();
+                return false;
+            default:
+                return false;
+        }
+    }
+
 }
